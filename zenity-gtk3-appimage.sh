566446ca8b703e06325b806578726cd27f126c89
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-04-21 00:52:50 +0200

    Update zenity-gtk3-appimage.sh

--- zenity-gtk3-appimage.sh
+++ zenity-gtk3-appimage.sh
@@ -9,10 +9,36 @@ REPO="https://gitlab.gnome.org/GNOME/zenity.git"
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 LIB4BIN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+ADWGTK3_VER="$(basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.com/lassekongo83/adw-gtk3/releases/latest))"
+ADWGTK3="https://github.com/lassekongo83/adw-gtk3/releases/download/$ADWGTK3_VER/adw-gtk3$ADWGTK3_VER.tar.xz"
 
 # Prepare AppDir
 mkdir -p ./AppDir
 cd ./AppDir
+
+cat >> ./AppRun << 'EOF'
+#!/bin/sh
+if command -v gsettings &> /dev/null; then
+  if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-dark'" ]; then
+    export GTK_THEME=adw-gtk3-dark
+  else
+    export GTK_THEME=adw-gtk3
+  fi
+elif command -v dconf &> /dev/null; then
+  if [ "$(dconf read /org/gnome/desktop/interface/color-scheme)" = "'prefer-dark'" ]; then
+    export GTK_THEME=adw-gtk3-dark
+  else
+    export GTK_THEME=adw-gtk3
+  fi
+else
+  export GTK_THEME=adw-gtk3
+fi
+
+CURRENTDIR="$(dirname "$(readlink -f "$0")")"
+exec "$CURRENTDIR/bin/zenity" "$@"
+EOF
+
+chmod +x ./AppRun
 
 git clone "$REPO" ./zenity && (
 	cd ./zenity
@@ -31,6 +57,13 @@ rm -rf ./zenity ./share/help
 sed -i 's|/usr/share|././/share|g' ./shared/bin/zenity
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' > ./.env
 
+# bundle theming, to make it look like GTK4 with dark theming support
+# (no smooth theme transition after theme change when zenity is opened though)
+wget "$ADWGTK3" -O $PWD/adw-gtk3-theme.tar.xz
+mkdir -p $PWD/usr/share/themes/
+tar -xf $PWD/adw-gtk3-theme.tar.xz -C $PWD/usr/share/themes/
+rm $PWD/adw-gtk3-theme.tar.xz
+
 # bundle dependencies
 wget "$LIB4BIN" -O ./lib4bin
 chmod +x ./lib4bin
@@ -40,7 +73,6 @@ xvfb-run -a -- ./lib4bin -p -v -s -k -e \
 	/usr/lib/gdk-pixbuf-*/*/*/* \
 	/usr/lib/gio/modules/libgvfsdbus*
 
-ln ./sharun ./AppRun
 ./sharun -g
 
 echo '[Desktop Entry]
